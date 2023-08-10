6469babed5dd16dacb4fa24f5f1cee80be2c934f
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-08-10 15:33:57 +0200

    Add NotFound component with navigation link
    Created a NotFound component to display a user-friendly "Page not found" message. Included a navigation link to the homepage using React Router's NavLink component for better user experience.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
 import Contact from "./pages/help/Contact";
+import NotFound from "./pages/NotFound.js";
+
 // layout
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
@@ -24,6 +26,8 @@ const router = createBrowserRouter(
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
       </Route>
+
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
