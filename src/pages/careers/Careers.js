51c046d88ca2ce33a05cbe43bfe0d6f8a9aecf69
Isa18V <131658612+Isa18V@users.noreply.github.com>
2023-08-11 17:14:03 +0200

    refurbishing

--- src/pages/careers/Careers.js
+++ src/pages/careers/Careers.js
@@ -3,9 +3,8 @@ import { Link, useLoaderData } from "react-router-dom";
 export default function Careers() {
   const careers = useLoaderData();
 
- 
   return (
-    <div className="careers"> 
+    <div className="careers">
       {careers.map((career) => (
         <Link to="/" key={career.id}>
           <p>{career.title}</p>
@@ -18,7 +17,7 @@ export default function Careers() {
 
 // data loader
 export const careersLoader = async () => {
-  const res = await fetch("http://localhost:4000/careers");
+  const res = await fetch("/db.json");
   const data = await res.json();
   return data;
 };
